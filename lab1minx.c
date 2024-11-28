87f9d09f2b8978a649b16c2831d341f5a1dcd96d
anusornW <78833104+anusornW@users.noreply.github.com>
2024-11-28 17:53:25 +0700

    another version by if-else

--- lab1minx.c
+++ lab1minx.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main() {
-	int num1=100;
+	int num1=10;
 	int num2=50;
 	if (num1>num2){
 		printf("num1 is greater than num2");
-	}
-	if (num2>num1) {
+	} else {
 		printf("num2 is greater than num1");
 	}
 }
